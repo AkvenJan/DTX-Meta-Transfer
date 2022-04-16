0d9797075d457f72cbe356c0b1bf7fc6f80bd83f
AkvenJan <akvenjan@gmail.com>
2022-04-16 18:37:39 +0300

    Testing hex/bytes representaion of file

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+import argparse
 import sys
 
-# argument with the path path to the file and file name
-input_file=open(sys.argv[1], 'rb')
+# all the available arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Path and file name of the input DTX to read meta from")
+parser.add_argument("-o","--output", help="Path and file name of the output DTX to transfer meto to")
+parser.add_argument("-r","--read", help="Option to just read the input file",action="store_true")
+args = parser.parse_args()
+
+# reading input file
+input_file=open(args.input, 'rb')
 
 # reading input file header in HEX format. Reading 164 bytes
-byte = input_file.read(164).hex()
+# byte = input_file.read(164).hex()
+# readint inpit file header in Python HEX format. Readint 164 bytes
+byte = input_file.read(164)
     
 # printing whole input file in HEX format for tests
 print(byte)
 
 # printing file type
-# we need to count symbols as nibbles (half of byte). If I say to read bytes from 3 and 4 (counting from 1), I need to set it as 2:4
-print(int(byte[:8],32))     #converting to int32
+# we need to count symbols as nibbles (half of byte) in case of using hex() convertion. If I say to read bytes from 3 and 4 (counting from 1), I need to set it as 2:4
+# so I removed this code to read file as raw bytes
+print(int.from_bytes(byte[0:4], 'little', signed=False))     #converting to int32
 
 # checking DTX_VERSION, signed int32. Should be -5
-DTX_VERSION = bytes.fromhex(byte[8:16])                             #converting to real python hex (like 0x)
-DTX_VERSION = int.from_bytes(DTX_VERSION, 'little', signed=True)    #converting python hex to signed int32
+DTX_VERSION=int.from_bytes(byte[4:8], 'little', signed=True)
 print(DTX_VERSION)
 
 # output_file=open(sys.argv[2], 'rb')
@@ -25,10 +35,15 @@ print(DTX_VERSION)
 # If LightFlag = 1, we need to transfer the ending of the file started with LIGHTDEFS definition
 # Since FileType and DTX_VERSION are always the same, we can start to transfer bytes from NumberOfMipmaps to NumberOfMipmapsUsed
 # Let's read them
-print(byte[24:52])
+print(byte[12:26])
 # Let's read BPP
-print(byte[52:54])
-print(int(byte[52:54],8))   #converting to int8
+print(byte[26:27])
+print(int.from_bytes(byte[26:27], 'little', signed=True))   #converting to int8
 
-# Let's read everything after BPP
+# Let's read everything after BPP to the end of header
 print(byte[54:328])
+
+if args.read:
+    print('Filename=' + args.input)
+    print('DTX_VERSION=' + str(DTX_VERSION) )
+    print('Size=' + str(int.from_bytes(byte[8:10], 'little', signed=False)) + 'x' + str(int.from_bytes(byte[10:12], 'little', signed=False)))
